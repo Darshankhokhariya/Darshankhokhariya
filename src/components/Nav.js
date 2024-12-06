0e19941e11528ba491871b92a0f7c57d31d1b1e1
Darshan khokhariya <darshankhokhariya26@gmail.com>
2024-12-06 18:27:55 +0530

    Changes in design

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -9,27 +9,25 @@ const Navbar = () => {
     };
 
     return (
-        <header className="bg-[#0f2027] w-full z-50 text-[#E3E4E6] shadow-lg sticky top-0">
+        <header className="bg-[#0f2027] w-full z-50 text-[#E3E4E6] shadow-lg sticky top-0 ">
             <div className="container mx-auto flex justify-between items-center py-4 px-6">
                 <h1 className="text-2xl font-bold font-heading">Darshan Khokhariya</h1>
                 <nav>
                     {/* Desktop Navbar */}
                     <ul className="hidden md:flex space-x-6">
                         <li><Link href="/" className="hover:text-primary text-sm uppercase">Home</Link></li>
-                        <li><Link href="/projects" className="hover:text-primary text-sm uppercase">Work</Link></li>
-                        <li><Link href="#services" className="hover:text-primary text-sm uppercase">About</Link></li>
+                        <li><Link href="/projects" className="hover:text-primary text-sm uppercase">Projects</Link></li>
                     </ul>
 
                     {/* Mobile Navbar */}
-                    <div className="md:hidden">
+                    <div className="md:hidden ">
                         <button onClick={toggleMenu} className="text-2xl">
                             {isMenuOpen ? "✕" : "☰"}
                         </button>
                         {isMenuOpen && (
                             <ul className="absolute top-16 left-0 w-full bg-[#0f2027] text-start px-10 space-y-4 py-4">
                                 <li><Link href="/" className="text-base text-[#E3E4E6] uppercase">Home</Link></li>
-                                <li><Link href="/projects" className="text-base text-[#E3E4E6] uppercase">Work</Link></li>
-                                <li><Link href="#services" className="text-base text-[#E3E4E6] uppercase">About</Link></li>
+                                <li><Link href="/projects" className="text-base text-[#E3E4E6] uppercase">Projects</Link></li>
                             </ul>
                         )}
                     </div>
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -18,7 +18,7 @@ const Portfolio = () => {
             ],
         },
         {
-            title: "Vendor management",
+            title: "Vendor Management",
             description: "Comprehensive Vendor Management System with role-based access, user authentication, and features to manage vendors, blogs, and events seamlessly.",
             images: [
                 "./images/canconnect/2.png",
@@ -35,57 +35,44 @@ const Portfolio = () => {
     ];
 
     return (
-        <div>
+        <div className="bg-gray-900 min-h-screen text-white">
             {/* Total Projects Section */}
             <section className="py-8">
-                <div className="container mx-auto px-4 sm:px-6 lg:px-8 text-center">
-                    <h2 className="text-3xl font-bold text-white">
-                        Total Projects Completed: {projects.length}
+                <div className="container mx-auto px-4 text-center">
+                    <h2 className="text-3xl font-bold">
+                        Completed Projects
                     </h2>
                 </div>
             </section>
 
             {/* Projects Section */}
-            <div className="container mx-auto px-4 sm:px-6 lg:px-8 py-12">
+            <div className="container mx-auto px-4 py-8 md:py-12 grid gap-12">
                 {projects.map((project, index) => (
                     <section
                         key={index}
-                        className="mb-12 rounded-lg shadow-lg overflow-hidden bg-gray-800"
+                        className="rounded-lg shadow-lg overflow-hidden bg-gray-800 grid grid-cols-1 md:grid-cols-2"
                     >
-                        <div className={`grid grid-cols-1 md:grid-cols-2 items-center gap-8 ${index % 2 === 0 ? 'order-1 md:order-1' : 'order-2 md:order-2'}`}>
-                            {/* Project Details */}
-                            <div className={`p-6 text-white space-y-4 ${index % 2 === 0 ? ' md:order-2' : '  md:order-1'}`}>
-                                <h2 className="text-2xl font-bold">{project.title}</h2>
-                                <p className="text-gray-300">{project.description}</p>
-                                <Link
-                                    href={`/projects/${project.title
-                                        .toLowerCase()
-                                        .replace(/\s+/g, "-")}`}
-                                    className="inline-block bg-gradient-to-r from-primary to-blue-500 text-white px-6 py-2 rounded-lg shadow-md hover:scale-105 transition-transform"
-                                >
-                                    View Details
-                                </Link>
-                            </div>
+                        {/* Image */}
+                        <div className="p-4 order-1 ">
+                            <img
+                                src={project.images[0]}
+                                alt={project.title}
+                                className="object-cover w-full h-64 md:h-[700px] rounded-lg"
+                            />
+                        </div>
 
-                            {/* Image Slider */}
-                            <div className={`p-4 ${index % 2 === 0 ? ' md:order-1' : 'md:order-2'}`}>
-                                <div className="swiper-container rounded-lg overflow-hidden">
-                                    <div className="swiper-wrapper">
-                                        {project.images.map((img, i) => (
-                                            <div
-                                                key={i}
-                                                className="swiper-slide h-[300px] md:h-[400px]"
-                                            >
-                                                <img
-                                                    src={img}
-                                                    alt={`Slide ${i + 1}`}
-                                                    className="object-cover w-full h-full"
-                                                />
-                                            </div>
-                                        ))}
-                                    </div>
-                                </div>
-                            </div>
+                        {/* Details */}
+                        <div className="p-6 order-2 space-y-4 flex flex-col justify-center">
+                            <h2 className="text-2xl font-bold">{project.title}</h2>
+                            <p className="text-gray-300">{project.description}</p>
+                            <Link
+                                href={`/projects/${project.title
+                                    .toLowerCase()
+                                    .replace(/\s+/g, "-")}`}
+                                className="inline-block bg-gradient-to-r from-primary to-blue-500 text-white px-6 py-2 rounded-lg shadow-md hover:scale-105 transition-transform w-max"
+                            >
+                                View Details
+                            </Link>
                         </div>
                     </section>
                 ))}
